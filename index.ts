b56fb9dea194d740cef80b6e68c39f5915bb0cb0
FannyIsPrettyCool <91889051+FannyIsPrettyCool@users.noreply.github.com>
2022-09-05 16:05:27 +0200

    2 files modified

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 function initMap(): void {
   const dth = { lat: 48.6863399, lng: 6.1715808 };
+  const centre = { lat: 48.688, lng: 6.17196 };
   const map = new google.maps.Map(
     document.getElementById('map') as HTMLElement,
     {
-      zoom: 15,
-      center: dth,
+      zoom: 17,
+      center: centre,
     }
   );
 
